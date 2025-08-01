a567f2e8ed5c1559dd85567a4a45acfd911c3116
OrDinaD <Vlad.vasilevskiy.07@gmail.com>
2025-08-01 15:19:32 +0300

    Initial Commit

---
+++ TextToBand/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextToBand
+//
+//  Created by Влад on 1.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextToBand/TextToBandApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToBandApp.swift
+//  TextToBand
+//
+//  Created by Влад on 1.08.25.
+//
+
+import SwiftUI
+
+@main
+struct TextToBandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
